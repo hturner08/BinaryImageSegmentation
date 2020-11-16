724bc7d686f2bd9666cdd3e7860010e9faf8286a
Turner <herbert.turner@milwaukeetool.com>
2020-11-16 03:45:01 -0600

    Windows compatible repository for image segmentation

---
+++ demo_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Image and model names
+TEST_IMG=ADE_val_00001519.jpg
+MODEL_NAME=ade20k-resnet50dilated-ppm_deepsup
+MODEL_PATH=ckpt/$MODEL_NAME
+RESULT_PATH=./
+
+ENCODER=$MODEL_NAME/encoder_epoch_20.pth
+DECODER=$MODEL_NAME/decoder_epoch_20.pth
+
+# Download model weights and image
+if [ ! -e $MODEL_PATH ]; then
+  mkdir -p $MODEL_PATH
+fi
+if [ ! -e $ENCODER ]; then
+  curl --output $MODEL_PATH/encoder_epoch_20.pth http://sceneparsing.csail.mit.edu/model/pytorch/$ENCODER
+fi
+if [ ! -e $DECODER ]; then
+  curl --output $MODEL_PATH/decoder_epoch_20.pth http://sceneparsing.csail.mit.edu/model/pytorch/$DECODER
+fi
+if [ ! -e $TEST_IMG ]; then
+  curl --output $RESULT_PATH/test.jpg http://sceneparsing.csail.mit.edu/data/ADEChallengeData2016/images/validation/$TEST_IMG
+fi
+
+if [ -z "$DOWNLOAD_ONLY" ]
+then
+
+# Inference
+python -u test.py \
+  --imgs $TEST_IMG \
+  --cfg config/ade20k-resnet50dilated-ppm_deepsup.yaml \
+  DIR $MODEL_PATH \
+  TEST.result ./ \
+  TEST.checkpoint epoch_20.pth
+
+fi
